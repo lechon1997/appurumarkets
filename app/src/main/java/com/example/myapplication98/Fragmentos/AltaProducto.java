26fe734f1a23c5c4e2907fbdaf93c16fc8785cca
emilianopinato <emilianopinato@gmail.com>
2021-06-01 18:34:15 -0300

    asd

--- app/src/main/java/com/example/myapplication98/Fragmentos/AltaProducto.java
+++ app/src/main/java/com/example/myapplication98/Fragmentos/AltaProducto.java
@@ -9,29 +9,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkError;
-import com.android.volley.NoConnectionError;
 import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.ServerError;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.example.myapplication98.Controladores.ControladorUsuario;
 import com.example.myapplication98.Controladores.ControladorVista;
 import com.example.myapplication98.Modelo.Departamento;
@@ -42,20 +35,18 @@ import com.example.myapplication98.WebService.MySingleton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.gson.Gson;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragmentLogin#newInstance} factory method to
+ * Use the {@link AltaProducto#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragmentLogin extends Fragment {
+public class AltaProducto extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -66,13 +57,15 @@ public class fragmentLogin extends Fragment {
     private ControladorUsuario CU = ControladorUsuario.getInstance();
     private View myView;
 
-    private Button btnIniciarSesion;
+    private Button altaP;
     private Button btnRegistrarse;
     private CheckBox cbRecuerdame;
+
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public fragmentLogin() {
+    public AltaProducto() {
         // Required empty public constructor
     }
 
@@ -82,11 +75,11 @@ public class fragmentLogin extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragmentLogin.
+     * @return A new instance of fragment AltaProducto.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragmentLogin newInstance(String param1, String param2) {
-        fragmentLogin fragment = new fragmentLogin();
+    public static AltaProducto newInstance(String param1, String param2) {
+        AltaProducto fragment = new AltaProducto();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -97,60 +90,33 @@ public class fragmentLogin extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        myView = inflater.inflate(R.layout.fragment_login, container, false);
-        return myView;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        btnIniciarSesion = (Button)view.findViewById(R.id.btniniciarSesion) ;
-        btnRegistrarse = (Button)view.findViewById(R.id.btnRegistrarse);
-        cbRecuerdame = (CheckBox)view.findViewById(R.id.checkBox);
-
-        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextInputEditText etu = view.findViewById(R.id.usuet);
-                TextInputEditText etp = view.findViewById(R.id.passusu);
-                String usu = etu.getText().toString();
-                String pass = etp.getText().toString();
-                autenticarUsuario(usu,pass);
-            }
-        });
-
-        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragmentConteiner,CV.getFGRegistroUsuario());
-                transaction.commit();
-            }
-        });
-    }
-
-    private void autenticarUsuario(String usu, String pass) {
+    private void autenticarUsuario(String nombreProducto, String descripcionProducto, String stock, String checkboxOferta, String checkboxTienePrecio, String tipoMoneda, String precioProducto,
+                                   String productosPorPersona, String porcentajeOfertaProducto, String estadoProducto, String publicacion) {
         //String url = "http://192.168.1.11/urumarkets/public/api/autenticarUsuario";
 
-        String LOGIN_REQUEST_URL = "http://192.168.1.11/urumarkets/public/api/autenticarUsuario";
+        String LOGIN_REQUEST_URL = "http://192.168.1.8/urumarkets/public/api/altaproductowbs";
         //altaproductowbs
         // JSON data
         JSONObject jsonObject = new JSONObject();
         try{
-            jsonObject.put("email", usu);
-            jsonObject.put("password", pass);
+            jsonObject.put("nombreProducto", nombreProducto);
+            jsonObject.put("descripcionProducto", descripcionProducto);
+            jsonObject.put("stock", stock);
+            jsonObject.put("checkboxOferta", checkboxOferta);
+            jsonObject.put("checkboxTienePrecio", checkboxTienePrecio);
+            jsonObject.put("tipoMoneda", tipoMoneda);
+            jsonObject.put("precioProducto", precioProducto);
+            jsonObject.put("productosPorPersona", productosPorPersona);
+            jsonObject.put("porcentajeOfertaProducto", porcentajeOfertaProducto);
+            jsonObject.put("estadoProducto", estadoProducto);
+            jsonObject.put("publicacion", publicacion);
         } catch (JSONException e){
             e.printStackTrace();
         }
@@ -162,52 +128,10 @@ public class fragmentLogin extends Fragment {
                 new Response.Listener<JSONObject>(){
                     @Override
                     public void onResponse(JSONObject response){
-
-
                         try {
                             JSONObject jo = response.getJSONObject("respuesta");
                             if(jo.optString("estado").equals("ok")){
-
-                                Usuario usuario = new Usuario();
-                                Departamento departamento = new Departamento();
-                                Localidad localidad = new Localidad();
-
-                                usuario.setId(jo.optInt("id"));
-                                usuario.setTipoUsuario(jo.optString("tipoUsu"));
-                                usuario.setPrimer_nombre(jo.optString("pnombre"));
-                                usuario.setSegundo_nombre(jo.optString("snombre"));
-                                usuario.setPrimer_apellido(jo.optString("papellido"));
-                                usuario.setSegundo_apellido(jo.optString("sapellido"));
-                                usuario.setCedula(jo.optString("cedula"));
-                                usuario.setTelefono(jo.getString("telefono"));
-                                usuario.setEmail(jo.getString("email"));
-
-                                departamento.setId(jo.optInt("idDepartamento"));
-                                departamento.setNombre(jo.optString("nomDepartamento"));
-
-                                localidad.setId(jo.optInt("idLocalidad"));
-                                localidad.setNombre(jo.optString("nomLocalidad"));
-
-                                usuario.setDepartamento(departamento);
-                                usuario.setLocalidad(localidad);
-                                CU.setUsuario(usuario);
-
-                                if(cbRecuerdame.isChecked()){
-                                    SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-                                    SharedPreferences.Editor editor = sharedPref.edit();
-                                    Gson gson = new Gson();
-                                    String jsonUsuario = gson.toJson(usuario);
-                                    editor.putString(jo.getString("email"),jsonUsuario);
-                                    editor.putString("usuarioRecordado",jo.getString("email"));
-                                    editor.putBoolean("recordado",true);
-                                    editor.commit();
-                                }
-
-
-                                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                                transaction.replace(R.id.fragmentConteiner,CV.getFGInicio());
-                                transaction.commit();
-
+                                Toast.makeText(getContext(),"creado correctamente",Toast.LENGTH_SHORT).show();
                             }else if (jo.optString("estado").equals("incorrecto"))
                                 Toast.makeText(getContext(),"incorrecto",Toast.LENGTH_SHORT).show();
                             else
@@ -215,7 +139,7 @@ public class fragmentLogin extends Fragment {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-            }
+                    }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -243,5 +167,72 @@ public class fragmentLogin extends Fragment {
 
         RequestQueue queue = MySingleton.getInstance(getContext()).getRequestQueue();
         MySingleton.getInstance(getContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_alta_producto, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //jsonObject.put("nombreProducto", nombreProducto);     ---
+        //jsonObject.put("descripcionProducto", descripcionProducto);   ---
+        //jsonObject.put("oferta", oferta);     ----
+        //jsonObject.put("checkboxOferta", checkboxOferta);         ---
+        //jsonObject.put("checkboxTienePrecio", checkboxTienePrecio);       ---
+        //jsonObject.put("tipoMoneda", tipoMoneda);                 ---
+        //jsonObject.put("precioProducto", precioProducto);         ---
+        //jsonObject.put("productosPorPersona", productosPorPersona);           ---
+        //jsonObject.put("porcentajeOfertaProducto", porcentajeOfertaProducto);     ----
+        //jsonObject.put("estadoProducto", estadoProducto);
+        //jsonObject.put("publicacion", publicacion);
+        altaP = (Button)view.findViewById(R.id.altaP) ;
+
+        altaP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextInputEditText servicio = view.findViewById(R.id.switch3);
+                TextInputEditText nombreart2 = view.findViewById(R.id.nombreart2);
+                TextInputEditText descripcion2 = view.findViewById(R.id.descripcion2);
+                TextInputEditText tieneprecio = view.findViewById(R.id.switch1);
+                TextInputEditText moneda = view.findViewById(R.id.planets_spinner);
+                TextInputEditText precio2 = view.findViewById(R.id.precio2);
+                TextInputEditText stock2 = view.findViewById(R.id.stock2);
+                TextInputEditText limistexpersona2 = view.findViewById(R.id.limistexpersona2);
+                TextInputEditText estaenoferta = view.findViewById(R.id.switch2);
+                TextInputEditText oferta2 = view.findViewById(R.id.oferta2);
+                TextInputEditText estado = view.findViewById(R.id.planets_spinner2);
+
+
+                String serviciotext = servicio.getText().toString();
+                String nombrearticulo = nombreart2.getText().toString();
+                String descripciontext = descripcion2.getText().toString();
+                String tienepreciotext = tieneprecio.getText().toString();
+                String monedatext = moneda.getText().toString();
+                String preciotext = precio2.getText().toString();
+                String stock = stock2.getText().toString();
+                String limistexpersona = limistexpersona2.getText().toString();
+                String estaenofertatext = estaenoferta.getText().toString();
+                String oferta = oferta2.getText().toString();
+                String estadotext = estado.getText().toString();
+
+                autenticarUsuario(nombrearticulo,descripciontext,stock,estaenofertatext, tienepreciotext,monedatext, preciotext,
+                        limistexpersona, oferta, estadotext, serviciotext);
+            }
+        });
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentConteiner,CV.getFGRegistroUsuario());
+                transaction.commit();
+            }
+        });
     }
 }
